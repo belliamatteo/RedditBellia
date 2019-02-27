1cdcc7d5154e9fde139a3c7fcc08414940cff281
EC2 Default User <ec2-user@ip-172-31-12-202.ec2.internal>
2019-02-27 13:51:51 +0000

    parte del 7

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { Article } from './article.module';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'card';
+ article:Article
+
+  constructor() {
+    this.article = new Article('Angular 2','http://angular.io',10);
+  }
+
+  voteUp() : Boolean {
+    this.article.votes += 1;
+    return false;
+  }
+
+  voteDown() : Boolean {
+    this.article.votes -= 1;
+    return false;
+  }
+
+  ngOnInit() {}
+}
